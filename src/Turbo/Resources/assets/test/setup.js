5b7129a6ebea0a68f0d9b9d802da5bb4ef6a49a3
Titouan Galopin <galopintitouan@gmail.com>
2021-04-04 18:14:37 +0200

    Fix UX Turbo usage without Streams

---
+++ src/Turbo/Resources/assets/test/setup.js
@@ -0,0 +1,10 @@
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import '@symfony/stimulus-testing/setup';
