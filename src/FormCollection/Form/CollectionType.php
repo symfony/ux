ae5d325f12b61e2cdd20c4f7b10a42be94b7fe40
Albin <83301974+stakovicz@users.noreply.github.com>
2021-05-03 11:59:15 +0200

    UX FormCollection

---
+++ src/FormCollection/Form/CollectionType.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\UX\FormCollection\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType as BaseCollectionType;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * @final
+ * @experimental
+ */
+class CollectionType extends AbstractType
+{
+    public function getParent()
+    {
+        return BaseCollectionType::class;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'button_add' => [
+                'text' => 'Add',
+                'attr' => ['class' => 'btn btn-outline-primary'],
+            ],
+            'button_delete' => [
+                'text' => 'Remove',
+                'attr' => ['class' => 'btn btn-outline-secondary'],
+            ],
+        ]);
+    }
+
+    public function finishView(FormView $view, FormInterface $form, array $options)
+    {
+        parent::finishView($view, $form, $options);
+
+        $view->vars['button_add'] = $options['button_add'];
+        $view->vars['button_delete'] = $options['button_delete'];
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'form_collection';
+    }
+}
