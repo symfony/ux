c1194dbb75ab0fb6999b19d313b7db6fd867c0f7
Tomas <norkunas.tom@gmail.com>
2022-05-06 06:51:03 +0300

    [TwigComponent] Allow to pass stringable object as non mapped component attribute
    Sometimes we have some value objects that are meant to be provided as stimulus controller values and when using ``{{ component('..', { 'data-item-id-param': objectUuid }) }}` it throws an exception that attribute must be scalar.
    
    Now it will allow to accept these objects.

--- src/TwigComponent/src/ComponentFactory.php
+++ src/TwigComponent/src/ComponentFactory.php
@@ -71,6 +71,10 @@ final class ComponentFactory
 
         // ensure remaining data is scalar
         foreach ($data as $key => $value) {
+            if ($value instanceof \Stringable) {
+                continue;
+            }
+
             if (!\is_scalar($value) && null !== $value) {
                 throw new \LogicException(sprintf('Unable to use "%s" (%s) as an attribute. Attributes must be scalar or null. If you meant to mount this value on "%s", make sure "$%1$s" is a writable property or create a mount() method with a "$%1$s" argument.', $key, get_debug_type($value), $component::class));
             }
