391c2eccdadbda6c5fd651134b34c6fc7a81dfdb
Michael Schmidt <ms@mtm.io>
2022-09-14 07:35:42 +0200

    [Vue] Allow multiple contexts
    Currently only one context is allowed. This way the generated Javascript always includes every Vue component and it is not possible to do proper code splitting.

--- src/Vue/Resources/assets/dist/register_controller.js
+++ src/Vue/Resources/assets/dist/register_controller.js
@@ -1,11 +1,17 @@
-function registerVueControllerComponents(context) {
+function registerVueControllerComponents(contexts) {
     const vueControllers = {};
     const importAllVueComponents = (r) => {
         r.keys().forEach((key) => (vueControllers[key] = r(key).default));
     };
-    importAllVueComponents(context);
+
+    [].concat(contexts).forEach((context) => importAllVueComponents(context));
+
     window.resolveVueComponent = (name) => {
-        const component = vueControllers[`./${name}.vue`];
+        const component = Object.values(
+            Object.fromEntries(
+                Object.entries(vueControllers).filter(([key]) => key.endsWith(`${name}.vue`)))
+        )[0];
+
         if (typeof component === 'undefined') {
             throw new Error('Vue controller "' + name + '" does not exist');
         }
---
+++ src/Vue/Resources/assets/test/fixtures/entry/Entry.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This entry is called {{ entry }}</h1>
+</template>
