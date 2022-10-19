9ee03b12c5524f8d5208be93c47d123d6cdc3d7a
Ryan Weaver <ryan@thatsquality.com>
2022-10-19 10:43:10 -0400

    removing accidental change of live component

--- ux.symfony.com/assets/bootstrap.js
+++ ux.symfony.com/assets/bootstrap.js
@@ -1,6 +1,5 @@
 import { startStimulusApp } from '@symfony/stimulus-bridge';
 import Clipboard from 'stimulus-clipboard'
-import LiveController from '../live_assets/dist/live_controller';
 
 // Registers Stimulus controllers from controllers.json and in the controllers/ directory
 export const app = startStimulusApp(require.context(
@@ -13,5 +12,4 @@ app.debug = process.env.NODE_ENV === 'development';
 
 app.register('clipboard', Clipboard);
 // register any custom, 3rd party controllers here
-app.register('live', LiveController);
 
