29b44144177d0f014b10bcf08561ef708d47636e
Thomas Choquet <thomas.choquet.pro@gmail.com>
2023-01-26 21:24:38 +0100

    [Svelte] Update build files

--- src/Svelte/assets/dist/render_controller.js
+++ src/Svelte/assets/dist/render_controller.js
@@ -6,7 +6,7 @@ class default_1 extends Controller {
         this.element.innerHTML = '';
         this.props = (_a = this.propsValue) !== null && _a !== void 0 ? _a : undefined;
         this.intro = (_b = this.introValue) !== null && _b !== void 0 ? _b : undefined;
-        this._dispatchEvent('svelte:connect');
+        this.dispatchEvent('connect');
         const Component = window.resolveSvelteComponent(this.componentValue);
         this._destroyIfExists();
         this.app = new Component({
@@ -15,13 +15,13 @@ class default_1 extends Controller {
             intro: this.intro,
         });
         this.element.root = this.app;
-        this._dispatchEvent('svelte:mount', {
+        this.dispatchEvent('mount', {
             component: Component,
         });
     }
     disconnect() {
         this._destroyIfExists();
-        this._dispatchEvent('svelte:unmount');
+        this.dispatchEvent('unmount');
     }
     _destroyIfExists() {
         if (this.element.root !== undefined) {
@@ -29,9 +29,9 @@ class default_1 extends Controller {
             delete this.element.root;
         }
     }
-    _dispatchEvent(name, payload = {}) {
+    dispatchEvent(name, payload = {}) {
         const detail = Object.assign({ componentName: this.componentValue, props: this.props, intro: this.intro }, payload);
-        this.element.dispatchEvent(new CustomEvent(name, { detail, bubbles: true }));
+        this.dispatch(name, { detail, prefix: 'svelte' });
     }
 }
 default_1.values = {
