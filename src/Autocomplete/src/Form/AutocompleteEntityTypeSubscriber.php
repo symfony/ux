c19877cdf9ad2930dbcba8abbbda2e620302ee7a
Aziz BenMallouk <azizbenmallouk4@gmail.com>
2023-02-02 10:16:58 +0000

    [Autocomplete] Fix setData in AutocompleteField

--- src/Autocomplete/src/Form/AutocompleteEntityTypeSubscriber.php
+++ src/Autocomplete/src/Form/AutocompleteEntityTypeSubscriber.php
@@ -43,7 +43,7 @@ final class AutocompleteEntityTypeSubscriber implements EventSubscriberInterface
         // pass to AutocompleteChoiceTypeExtension
         $options['autocomplete'] = true;
         $options['autocomplete_url'] = $this->autocompleteUrl;
-        unset($options['searchable_fields'], $options['security'], $options['filter_query']);
+        unset($options['searchable_fields'], $options['security'], $options['filter_query'], $options['data_class']);
 
         $form->add('autocomplete', EntityType::class, $options);
     }
