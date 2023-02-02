d85f26de3738c3164b1e3e4bc61bca88e1cf22ad
Javier Eguiluz <javier.eguiluz@gmail.com>
2023-02-02 17:07:10 +0100

    [Website] Simplify some contents in the Swup example

--- ux.symfony.com/code_snippets/_SwupController.php
+++ ux.symfony.com/code_snippets/_SwupController.php
@@ -11,18 +11,19 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class _SwupController extends AbstractController
 {
-    #[Route('/swup/{page<\d+>}', name: 'app_swup')]
-    public function swup(PackageRepository $packageRepository, int $page = 1): Response
+    #[Route('/packages/{page<\d+>}', name: 'app_swup')]
+    public function packagesIndex(PackageRepository $packageRepository, int $page = 1): Response
     {
-        // pagination links are just an example: swup works with any "a" tags!
-        $pagerfanta = Pagerfanta::createForCurrentPageWithMaxPerPage(
+        // this is a normal Symfony controller and pagination links are just an example:
+        // swup works with any "a" tags, so you don't have to make any changes in your code.
+        $pager = Pagerfanta::createForCurrentPageWithMaxPerPage(
             new ArrayAdapter($packageRepository->findAll()),
             $page,
             4
         );
 
-        return $this->render('ux_packages/swup.html.twig', [
-            'pager' => $pagerfanta,
+        return $this->render('packages/index.html.twig', [
+            'pager' => $pager,
         ]);
     }
 }
