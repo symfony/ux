f0a4fe4d2c325a01fd2ba30b324afa8d25308dc5
Ryan Weaver <ryan@thatsquality.com>
2023-05-03 06:33:04 -0400

    [Twig] Working around bug where the new lexer causes the extension set to load too early

--- src/TwigComponent/src/Twig/ComponentLexer.php
+++ src/TwigComponent/src/Twig/ComponentLexer.php
@@ -11,6 +11,7 @@
 
 namespace Symfony\UX\TwigComponent\Twig;
 
+use Twig\Environment;
 use Twig\Lexer;
 use Twig\Source;
 use Twig\TokenStream;
@@ -26,8 +27,27 @@ use Twig\TokenStream;
  */
 class ComponentLexer extends Lexer
 {
+    private Environment $env;
+    private array $options;
+    private bool $isConstructed = false;
+
+    public function __construct(Environment $env, array $options = [])
+    {
+        $this->env = $env;
+        $this->options = $options;
+    }
+
     public function tokenize(Source $source): TokenStream
     {
+        // odd behavior is because the parent constructor causes the extension set
+        // to be initialized earlier than it normally would be.
+        // https://github.com/symfony/ux/issues/835
+        if (!$this->isConstructed) {
+            parent::__construct($this->env, $this->options);
+
+            $this->isConstructed = true;
+        }
+
         $preLexer = new TwigPreLexer();
         $preparsed = $preLexer->preLexComponents($source->getCode());
 
