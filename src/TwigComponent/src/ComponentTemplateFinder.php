34534685fe437ae6cee578b2fcde379289c1fb03
matheo <mdaninos@apple.com>
2023-07-06 13:54:30 +0200

    ComponentTemplateFinder

--- src/TwigComponent/src/ComponentTemplateFinder.php
+++ src/TwigComponent/src/ComponentTemplateFinder.php
@@ -1,8 +1,48 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\UX\TwigComponent;
 
-interface ComponentTemplateFinder
-{
+use Twig\Environment;
 
+/**
+ * @author Matheo Daninos <matheo.daninos@gmail.com>
+ */
+class ComponentTemplateFinder implements ComponentTemplateFinderInterface
+{
+    public function __construct(
+        private Environment $environment
+    ) {}
+
+    public function findAnonymousComponentTemplate(string $name): ?string
+    {
+        $loader = $this->environment->getLoader();
+        $componentPath = rtrim(str_replace(':', '/', $name));
+
+        if ($loader->exists($componentPath)) {
+            return $componentPath;
+        }
+
+        if ($loader->exists($componentPath.'.html.twig')) {
+            return $componentPath.'.html.twig';
+        }
+
+        if ($loader->exists('components/'.$componentPath)) {
+            return 'components/'.$componentPath;
+        }
+
+        if ($loader->exists('components/'.$componentPath.'.html.twig')) {
+            return 'components/'.$componentPath.'.html.twig';
+        }
+
+        return null;
+    }
 }
