0950bfa271d655f68250a4f9e48ab88ae51bb73c
Ryan Weaver <ryan@symfonycasts.com>
2023-09-08 06:47:13 -0400

    [Site] Fix minor bug where theme didn't change on first click

--- ux.symfony.com/assets/controllers/theme-switcher-controller.js
+++ ux.symfony.com/assets/controllers/theme-switcher-controller.js
@@ -2,7 +2,12 @@ import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
     switch() {
-        const theme = localStorage.getItem('user-theme') === 'dark' ? 'light' : 'dark';
+        let currentTheme = localStorage.getItem('user-theme');
+        if (!currentTheme) {
+            currentTheme = document.documentElement.getAttribute('data-bs-theme');
+        }
+
+        const theme = currentTheme === 'dark' ? 'light' : 'dark';
         localStorage.setItem('user-theme', theme);
         document.documentElement.setAttribute('data-bs-theme', theme);
     }
