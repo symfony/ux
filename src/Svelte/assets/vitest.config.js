cf29587c4a3002550b87c94278a350c922aa2ebe
Ryan Weaver <ryan@symfonycasts.com>
2023-10-21 14:46:37 -0400

    Replacing require.context polyfill with a simpler implementation

--- src/Svelte/assets/vitest.config.js
+++ src/Svelte/assets/vitest.config.js
@@ -1,6 +1,6 @@
 // vitest.config.ts
 import { defineConfig, mergeConfig } from 'vitest/config';
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 import configShared from '../../../vitest.config.js'
 
 export default mergeConfig(
