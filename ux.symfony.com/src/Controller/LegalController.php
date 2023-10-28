517da163970ff530e70dd1aa89f2515c21b10f7e
Simon André <smn.andre@gmail.com>
2023-10-29 00:18:45 +0200

    Add privacy page

---
+++ ux.symfony.com/src/Controller/LegalController.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LegalController extends AbstractController
+{
+    #[Route('/privacy', name: 'app_legal_privacy')]
+    public function privacy(): Response
+    {
+        return $this->render('legal/privacy.html.twig');
+    }
+}
