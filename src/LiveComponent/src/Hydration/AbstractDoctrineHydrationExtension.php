d8feb0232f38f645f25fc2227d5d120427f7d179
jpvdw <info@jpvdw.nl>
2023-12-27 00:09:55 +0100

    fix code block

--- src/LiveComponent/src/Hydration/AbstractDoctrineHydrationExtension.php
+++ src/LiveComponent/src/Hydration/AbstractDoctrineHydrationExtension.php
@@ -1,20 +1,18 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Symfony\UX\LiveComponent\Hydration;
 
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Doctrine\Persistence\ObjectManager;
 
+/**
+ * @author Jean-Paul van der Wegen <info@jpvdw.nl>
+ *
+ * @experimental
+ *
+ * @internal
+ */
 abstract class AbstractDoctrineHydrationExtension
 {
     /**
