dfd6a2aafdf23ba92ff0737995e73716a7a21b21
Simon André <smn.andre@gmail.com>
2024-03-09 05:18:00 +0100

    [Icons] Add InMemory registry to allow easier tests

---
+++ src/Icons/src/Registry/InMemoryIconRegistry.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\UX\Icons\Registry;
+
+use Symfony\UX\Icons\Exception\IconNotFoundException;
+use Symfony\UX\Icons\IconRegistryInterface;
+use Symfony\UX\Icons\Svg\Icon;
+
+/**
+ * @author Simon André <smn.andre@gmail.com>
+ */
+final class InMemoryIconRegistry implements IconRegistryInterface
+{
+    public function __construct(
+        /**
+         * @var array<string, Icon> $icons
+         */
+        private array $icons = [],
+    ) {
+    }
+
+    public function set(string $name, Icon $icon): void
+    {
+        $this->icons[$name] = $icon;
+    }
+
+    public function get(string $name): Icon
+    {
+        return $this->icons[$name] ?? throw new IconNotFoundException(sprintf('Icon "%s" not found.', $name));
+    }
+}
