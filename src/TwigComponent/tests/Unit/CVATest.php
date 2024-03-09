600685c95bed7f6ae65cac2e10b06a0b3f8c2bb9
Matheo Daninos <matheo.daninos@gmail.com>
2024-03-09 16:30:38 +0100

    CVA: Allow array for base, variants and compound

--- src/TwigComponent/tests/Unit/CVATest.php
+++ src/TwigComponent/tests/Unit/CVATest.php
@@ -88,6 +88,24 @@ class CVATest extends TestCase
             'text-primary text-sm',
         ];
 
+        yield 'base array' => [
+            [
+                'base' => ['font-semibold', 'border', 'rounded'],
+                'variants' => [
+                    'colors' => [
+                        'primary' => 'text-primary',
+                        'secondary' => 'text-secondary',
+                    ],
+                    'sizes' => [
+                        'sm' => 'text-sm',
+                        'md' => 'text-md',
+                        'lg' => 'text-lg',
+                    ],
+                ]],
+            ['colors' => 'primary', 'sizes' => 'sm'],
+            'font-semibold border rounded text-primary text-sm',
+        ];
+
         yield 'no recipes match' => [
             [
                 'base' => 'font-semibold border rounded',
@@ -124,6 +142,25 @@ class CVATest extends TestCase
             ],
             ['colors' => 'primary', 'sizes' => 'sm'],
             'font-semibold border rounded text-primary text-sm',
+        ];
+
+        yield 'simple variants as array' => [
+            [
+                'base' => 'font-semibold border rounded',
+                'variants' => [
+                    'colors' => [
+                        'primary' => ['text-primary', 'uppercase'],
+                        'secondary' => ['text-secondary', 'uppercase'],
+                    ],
+                    'sizes' => [
+                        'sm' => 'text-sm',
+                        'md' => 'text-md',
+                        'lg' => 'text-lg',
+                    ],
+                ],
+            ],
+            ['colors' => 'primary', 'sizes' => 'sm'],
+            'font-semibold border rounded text-primary uppercase text-sm',
         ];
 
         yield 'simple variants with custom' => [
@@ -169,6 +206,32 @@ class CVATest extends TestCase
             ],
             ['colors' => 'primary', 'sizes' => 'sm'],
             'font-semibold border rounded text-primary text-sm text-red-500',
+        ];
+
+        yield 'compound variants as array' => [
+            [
+                'base' => 'font-semibold border rounded',
+                'variants' => [
+                    'colors' => [
+                        'primary' => 'text-primary',
+                        'secondary' => 'text-secondary',
+                    ],
+                    'sizes' => [
+                        'sm' => 'text-sm',
+                        'md' => 'text-md',
+                        'lg' => 'text-lg',
+                    ],
+                ],
+                'compounds' => [
+                    [
+                        'colors' => ['primary'],
+                        'sizes' => ['sm'],
+                        'class' => ['text-red-500', 'bold'],
+                    ],
+                ],
+            ],
+            ['colors' => 'primary', 'sizes' => 'sm'],
+            'font-semibold border rounded text-primary text-sm text-red-500 bold',
         ];
 
         yield 'multiple compound variants' => [
