c2dcd14d18dda11e8e5728763a3a36a9edf531f9
Kevin Bond <kevinbond@gmail.com>
2024-04-16 18:43:58 -0400

    [Site] improvements/fixes

--- ux.symfony.com/assets/controllers/code-highlighter-controller.js
+++ ux.symfony.com/assets/controllers/code-highlighter-controller.js
@@ -4,10 +4,14 @@ import hljs_javascript from 'highlight.js/lib/languages/javascript';
 import hljs_php from 'highlight.js/lib/languages/php';
 import hljs_xml from 'highlight.js/lib/languages/xml';
 import hljs_twig from 'highlight.js/lib/languages/twig';
+import hljs_yaml from 'highlight.js/lib/languages/yaml';
+import hljs_diff from 'highlight.js/lib/languages/diff';
 
 hljs.registerLanguage('javascript', hljs_javascript);
 hljs.registerLanguage('php', hljs_php);
 hljs.registerLanguage('twig', hljs_twig);
+hljs.registerLanguage('yaml', hljs_yaml);
+hljs.registerLanguage('diff', hljs_diff);
 // xml is the language used for HTML
 hljs.registerLanguage('xml', hljs_xml);
 
