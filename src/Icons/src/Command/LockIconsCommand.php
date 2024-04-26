017a8db88fdceb28653f759aa441d76daa26bc74
Simon André <smn.andre@gmail.com>
2024-04-26 17:59:21 +0200

    [Icons] Fix warning when Iconset license URL is null
    Fix #1785

--- src/Icons/src/Command/LockIconsCommand.php
+++ src/Icons/src/Command/LockIconsCommand.php
@@ -88,7 +88,7 @@ final class LockIconsCommand extends Command
                 " <fg=bright-green;options=bold>✓</> Imported <fg=bright-white;bg=black>%s:</><fg=bright-magenta;bg=black;options>%s</> (License: <href=%s>%s</>). Render with: <comment>{{ ux_icon('%s') }}</comment>",
                 $prefix,
                 $name,
-                $license['url'],
+                $license['url'] ?? '#',
                 $license['title'],
                 $icon,
             ));
