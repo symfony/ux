ce503ffc138d57121d8a22b3eb3fb316963e65d6
YummYume <yummyume@users.noreply.github.com>
2024-06-15 21:22:01 +0200

    Revert typing fix for hooks (done in another pr)

--- src/LiveComponent/assets/dist/HookManager.d.ts
+++ src/LiveComponent/assets/dist/HookManager.d.ts
@@ -1,7 +1,6 @@
-import type { ComponentHookName, ComponentHookCallback } from './Component';
 export default class {
     private hooks;
-    register<T extends string | ComponentHookName = ComponentHookName>(hookName: T, callback: ComponentHookCallback<T>): void;
-    unregister<T extends string | ComponentHookName = ComponentHookName>(hookName: T, callback: ComponentHookCallback<T>): void;
-    triggerHook<T extends string | ComponentHookName = ComponentHookName>(hookName: T, ...args: Parameters<ComponentHookCallback<T>>): void;
+    register(hookName: string, callback: (...args: any[]) => void): void;
+    unregister(hookName: string, callback: (...args: any[]) => void): void;
+    triggerHook(hookName: string, ...args: any[]): void;
 }
--- src/LiveComponent/assets/src/HookManager.ts
+++ src/LiveComponent/assets/src/HookManager.ts
@@ -1,21 +1,13 @@
-import type { ComponentHookName, ComponentHookCallback } from './Component';
-
 export default class {
-    private hooks: Map<ComponentHookName | string, Array<(...args: any[]) => void>> = new Map();
+    private hooks: Map<string, Array<(...args: any[]) => void>> = new Map();
 
-    register<T extends string | ComponentHookName = ComponentHookName>(
-        hookName: T,
-        callback: ComponentHookCallback<T>
-    ): void {
+    register(hookName: string, callback: (...args: any[]) => void): void {
         const hooks = this.hooks.get(hookName) || [];
         hooks.push(callback);
         this.hooks.set(hookName, hooks);
     }
 
-    unregister<T extends string | ComponentHookName = ComponentHookName>(
-        hookName: T,
-        callback: ComponentHookCallback<T>
-    ): void {
+    unregister(hookName: string, callback: (...args: any[]) => void): void {
         const hooks = this.hooks.get(hookName) || [];
         const index = hooks.indexOf(callback);
         if (index === -1) {
@@ -26,10 +18,7 @@ export default class {
         this.hooks.set(hookName, hooks);
     }
 
-    triggerHook<T extends string | ComponentHookName = ComponentHookName>(
-        hookName: T,
-        ...args: Parameters<ComponentHookCallback<T>>
-    ): void {
+    triggerHook(hookName: string, ...args: any[]): void {
         const hooks = this.hooks.get(hookName) || [];
         hooks.forEach((callback) => callback(...args));
     }
