5650ecf77b2e4ceebece45f4f8bea6ca98924051
Christoph Grabenstein <christoph.grabenstein@qossmic.com>
2024-07-15 09:50:13 +0200

    Allow updates in arrays of DTOs

---
+++ src/LiveComponent/tests/Fixtures/Dto/HoldsArrayOfDtos.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Symfony\UX\LiveComponent\Tests\Fixtures\Dto;
+
+final class HoldsArrayOfDtos
+{
+    /**
+     * @var Address[] $addresses
+     */
+    public array $addresses;
+}
