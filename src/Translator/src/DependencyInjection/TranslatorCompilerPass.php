abfa6c939176d473cbc2471cb25baf07aaf50724
Hugo Alliaume <hugo@alliau.me>
2024-07-19 08:18:08 +0200

    [Translator] Fix CS on TranslatorCompilerPass

--- src/Translator/src/DependencyInjection/TranslatorCompilerPass.php
+++ src/Translator/src/DependencyInjection/TranslatorCompilerPass.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\UX\Translator\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
@@ -25,6 +34,7 @@ class TranslatorCompilerPass implements CompilerPassInterface
         if (!is_subclass_of($translator->getClass(), TranslatorBagInterface::class)) {
             return false;
         }
+
         return true;
     }
 }
