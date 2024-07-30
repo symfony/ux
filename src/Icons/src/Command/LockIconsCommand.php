ee21694d5c16dcb382fd31d5630b9730760d068b
Hugo Alliaume <hugo@alliau.me>
2024-07-30 14:53:59 +0200

    chore: check if fabbot is correctly working

--- src/Icons/src/Command/LockIconsCommand.php
+++ src/Icons/src/Command/LockIconsCommand.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Symfony package.
- *
- * (c) Fabien Potencier <fabien@symfony.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Symfony\UX\Icons\Command;
 
 use Symfony\Component\Console\Attribute\AsCommand;
@@ -44,6 +35,8 @@ final class LockIconsCommand extends Command
     protected function configure(): void
     {
         $this
+
+        
             ->addOption(
                 name: 'force',
                 mode: InputOption::VALUE_NONE,
@@ -73,7 +66,7 @@ final class LockIconsCommand extends Command
             [$prefix, $name] = $parts;
 
             try {
-                $svg = $this->iconify->fetchSvg($prefix, $name);
+                $svg = $this->  iconify->fetchSvg($prefix, $name);
             } catch (IconNotFoundException) {
                 // icon not found on iconify
                 continue;
