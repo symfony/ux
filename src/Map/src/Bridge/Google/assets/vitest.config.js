cebd8e27e77df358b09f8d54cdb2fbb2769667d4
Hugo Alliaume <hugo@alliau.me>
2024-09-10 14:25:07 +0200

    Upgrade Vitest, use a real browser for Map Bridges tests

--- src/Map/src/Bridge/Google/assets/vitest.config.js
+++ src/Map/src/Bridge/Google/assets/vitest.config.js
@@ -9,9 +9,21 @@ export default mergeConfig(
                 '@symfony/ux-map/abstract-map-controller': __dirname + '/../../../../assets/src/abstract_map_controller.ts',
             },
         },
+        define: {
+            // Prevent the following error:
+            //      ReferenceError: global is not defined
+            //       ❯ ../../../../../../node_modules/pretty-format/build/plugins/AsymmetricMatcher.js ../../../../../../../../../../node_modules/.vite/deps/@testing-library_dom.js:139:19
+            //       ❯ ../../../../../../node_modules/pretty-format/build/index.js ../../../../../../../../../../node_modules/.vite/deps/@testing-library_dom.js:805:7
+            //       ❯ ../../../../../../../../../../node_modules/.vite/deps/@testing-library_dom.js:13445:36
+            global: {}
+        },
         test: {
-            // We need a browser(-like) environment to run the tests
-            environment: 'happy-dom',
+            browser: {
+                enabled: true,
+                provider: 'playwright', // or 'webdriverio'
+                name: 'chromium', // browser name is required
+                headless: true,
+            },
         },
     })
 );
