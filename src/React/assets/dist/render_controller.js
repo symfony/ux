bb6471bf91cd34992ab542a11cb2566ab032a882
Hugo Alliaume <hugo@alliau.me>
2024-09-28 00:34:21 +0200

    Update some dependencies to fix some issues from Dependabot

--- src/React/assets/dist/render_controller.js
+++ src/React/assets/dist/render_controller.js
@@ -2,23 +2,41 @@ import React from 'react';
 import require$$0 from 'react-dom';
 import { Controller } from '@hotwired/stimulus';
 
-var createRoot;
+var client = {};
 
-var m = require$$0;
-if (process.env.NODE_ENV === 'production') {
-  createRoot = m.createRoot;
-  m.hydrateRoot;
-} else {
-  var i = m.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
-  createRoot = function(c, o) {
-    i.usingClientEntryPoint = true;
-    try {
-      return m.createRoot(c, o);
-    } finally {
-      i.usingClientEntryPoint = false;
-    }
-  };
+var hasRequiredClient;
+
+function requireClient () {
+	if (hasRequiredClient) return client;
+	hasRequiredClient = 1;
+
+	var m = require$$0;
+	if (process.env.NODE_ENV === 'production') {
+	  client.createRoot = m.createRoot;
+	  client.hydrateRoot = m.hydrateRoot;
+	} else {
+	  var i = m.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED;
+	  client.createRoot = function(c, o) {
+	    i.usingClientEntryPoint = true;
+	    try {
+	      return m.createRoot(c, o);
+	    } finally {
+	      i.usingClientEntryPoint = false;
+	    }
+	  };
+	  client.hydrateRoot = function(c, h, o) {
+	    i.usingClientEntryPoint = true;
+	    try {
+	      return m.hydrateRoot(c, h, o);
+	    } finally {
+	      i.usingClientEntryPoint = false;
+	    }
+	  };
+	}
+	return client;
 }
+
+var clientExports = requireClient();
 
 class default_1 extends Controller {
     connect() {
@@ -45,7 +63,7 @@ class default_1 extends Controller {
     _renderReactElement(reactElement) {
         const element = this.element;
         if (!element.root) {
-            element.root = createRoot(this.element);
+            element.root = clientExports.createRoot(this.element);
         }
         element.root.render(reactElement);
     }
