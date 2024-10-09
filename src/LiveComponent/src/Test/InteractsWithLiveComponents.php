5b874d65a4340e6c3d83e4b008282c8e44ac6785
Marcus Stöhr <mstoehr@brainbits.net>
2024-10-09 19:53:54 +0200

    fix: add empty request to help with live component tests

--- src/LiveComponent/src/Test/InteractsWithLiveComponents.php
+++ src/LiveComponent/src/Test/InteractsWithLiveComponents.php
@@ -13,6 +13,10 @@ namespace Symfony\UX\LiveComponent\Test;
 
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\Storage\MockFileSessionStorageFactory;
 use Symfony\UX\TwigComponent\ComponentFactory;
 
 /**
@@ -32,6 +36,19 @@ trait InteractsWithLiveComponents
 
         if (!$metadata->get('live')) {
             throw new \LogicException(\sprintf('The "%s" component is not a live component.', $name));
+        }
+
+        /** @var RequestStack $requestStack */
+        $requestStack = self::getContainer()->get(RequestStack::class);
+        if (null === $requestStack->getMainRequest() && null === $requestStack->getCurrentRequest()) {
+            $request = new Request();
+
+            /** @var MockFileSessionStorageFactory $sessionFactory */
+            $sessionFactory = self::getContainer()->get('session.storage.factory.mock_file');
+            $session = new Session($sessionFactory->createStorage($request));
+
+            $request->setSession($session);
+            $requestStack->push($request);
         }
 
         return new TestLiveComponent(
