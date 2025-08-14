f7117f501abbf45c7ed0fcade14fe34ffed1ee54
Hugo Alliaume <hugo@alliau.me>
2025-08-14 23:25:25 +0200

    [CI] Fix highest dev jobs

--- src/Autocomplete/tests/Fixtures/Kernel.php
+++ src/Autocomplete/tests/Fixtures/Kernel.php
@@ -81,6 +81,13 @@ final class Kernel extends BaseKernel
             public function process(ContainerBuilder $container): void
             {
                 $container->removeDefinition('doctrine.orm.listeners.pdo_session_handler_schema_listener');
+
+                if (\PHP_VERSION_ID >= 80400) {
+                    // Workaround for `RuntimeException: Unable to create the Doctrine Proxy directory "". in vendor/symfony/doctrine-bridge/CacheWarmer/ProxyCacheWarmer.php:49`
+                    // when running PHP 8.4 and Doctrine ORM 3.5+.
+                    $container->getDefinition('doctrine.orm.default_configuration')
+                        ->addMethodCall('setProxyDir', ['%doctrine.orm.proxy_dir%']);
+                }
             }
         }, PassConfig::TYPE_BEFORE_OPTIMIZATION, 1);
     }
@@ -126,6 +133,9 @@ final class Kernel extends BaseKernel
             ];
         }
         if (null !== $doctrineBundleVersion = InstalledVersions::getVersion('doctrine/doctrine-bundle')) {
+            if (version_compare($doctrineBundleVersion, '2.8.0', '>=')) {
+                $doctrineConfig['orm']['enable_lazy_ghost_objects'] = true;
+            }
             if (\PHP_VERSION_ID >= 80400 && version_compare($doctrineBundleVersion, '2.15.0', '>=')) {
                 $doctrineConfig['orm']['enable_native_lazy_objects'] = true;
             }
