4949e620a6eee78fc085746a08a5b1170a893587
Hugo Alliaume <hugo@alliau.me>
2025-09-20 13:14:31 +0200

    Refactor "test_package.sh" to its original purpose, add multiples checks for packages definition

--- bin/unit_test_package.sh
+++ bin/unit_test_package.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script is used to test an UX package.
+# This script is used to unit test assets from an UX package.
 # It also handle the case where a package has multiple versions of a peerDependency defined.
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
@@ -22,13 +22,6 @@ if [ ! -d "$location" ]; then
 fi
 
 shift
-case "$1" in
-  --unit) testType="unit" ;;
-  --browser) testType="browser" ;;
-  *) echo "Unknown test type: $2. Please use --unit or --browser."; exit 1 ;;
-esac
-
-shift
 args=("$@")
 
 # Check if jq is installed
@@ -38,15 +31,9 @@ if ! command -v jq &> /dev/null; then
 fi
 
 runTestSuite() {
-    if [ "$testType" == "unit" ]; then
-      echo -e "🧪  Running unit tests for $workspace...\n"
-      # Using "cd" instead of "pnpm --filter" prevent rendering issues in GitHub Actions that does not support nested groups
-      (cd "$location"; pnpm exec vitest --run "${args[@]}") || { all_tests_passed=false; }
-    elif [ "$testType" == "browser" ]; then
-      echo -e "🧪  Running browser tests for $workspace...\n"
-      # Using "cd" instead of "pnpm --filter" prevent rendering issues in GitHub Actions that does not support nested groups
-      (cd "$location"; pnpm exec playwright test "${args[@]}") || { all_tests_passed=false; }
-    fi
+  echo -e "🧪  Running unit tests for $workspace...\n"
+  # Using "cd" instead of "pnpm --filter" prevent rendering issues in GitHub Actions that does not support nested groups
+  (cd "$location"; pnpm exec vitest --run "${args[@]}") || { all_tests_passed=false; }
 }
 
 processWorkspace() {
@@ -68,30 +55,6 @@ processWorkspace() {
     fi
 
     echo -e "⏳  Processing workspace $workspace at location $location...\n"
-
-    # TODO: Refactor the `test_package.sh` script to its simple form:  only run unit tests for a given UX package,
-    # The rest will be moved to `pnpm` scripts or other CI checks
-    if [ "$testType" == "browser" ]; then
-      runTestSuite
-    fi
-
-    echo "⚙️  Checking '$package_json_path' for peerDependencies and importmap dependencies to have the same version"
-    deps=$(jq -r '.peerDependencies | keys[]' "$package_json_path")
-    for library in $deps; do
-        version=$(jq -r ".peerDependencies.\"$library\"" "$package_json_path")
-        importmap_version=$(jq -r ".symfony.importmap.\"$library\" | if type == \"string\" then . else .version end" "$package_json_path")
-
-        if [ "$importmap_version" == null ]; then
-            echo " ⚠  No importmap version found for $library in $package_json_path, skipping..."
-            continue
-        fi
-
-        if [ "$version" != "$importmap_version" ]; then
-            echo " ⚠  Version mismatch for $library: $version (peerDependencies) vs $importmap_version (importmap)"
-            echo " ⚠  You need to match the version of the \"peerDependency\" with the version in the \"importmap\""
-            exit 1
-        fi
-    done
 
     echo "⚙️  Checking '$package_json_path' for peerDependencies with multiple versions defined"
     deps_with_multiple_versions=$(jq -r '.peerDependencies | to_entries[] | select(.value | contains("||")) | .key' "$package_json_path")
