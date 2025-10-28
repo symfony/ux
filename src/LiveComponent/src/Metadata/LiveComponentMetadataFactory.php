49fa189bb58db122985110c1308ce1cd835e64f7
Kevin Sliedrecht <kevin@made.frl>
2025-10-28 09:34:43 +0100

    Fix liveProp union type

--- src/LiveComponent/src/Metadata/LiveComponentMetadataFactory.php
+++ src/LiveComponent/src/Metadata/LiveComponentMetadataFactory.php
@@ -84,7 +84,7 @@ class LiveComponentMetadataFactory implements ResetInterface
     {
         $reflectionType = $property->getType();
         if ($reflectionType instanceof \ReflectionUnionType || $reflectionType instanceof \ReflectionIntersectionType) {
-            throw new \LogicException(\sprintf('Union or intersection types are not supported for LiveProps. You may want to change the type of property "%s" in "%s".', $property->getName(), $property->getDeclaringClass()->getName()));
+            return new LivePropMetadata($property->getName(), $liveProp, Type::mixed());
         }
 
         // BC layer when "symfony/type-info" is not available
