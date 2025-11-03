6e44ad9191473d8da6ace55e2614ab694489d953
Marcus Stöhr <marcus.stoehr@atvantage.com>
2025-11-03 07:52:23 +0100

    [Autocomplete] Fix issue where TomSelect could already been initialized on an element

---
+++ apps/e2e/src/Form/Type/MovieAutocompleteType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class MovieAutocompleteType extends AbstractType
+{
+    public function __construct(
+        private UrlGeneratorInterface $urlGenerator
+    ) {
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'autocomplete' => true,
+            'autocomplete_url' => $this->urlGenerator->generate('test_autocomplete_movie'),
+            'tom_select_options' => [
+                'maxOptions' => null,
+            ],
+            'attr' => [
+                'data-test-id' => 'movie-autocomplete',
+                'data-controller' => 'movie-autocomplete',
+            ],
+        ]);
+    }
+
+    public function getParent(): string
+    {
+        return TextType::class;
+    }
+}
