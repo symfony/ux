b1af2214189f7acc81b7e516c2e5012ddeaffe97
Hugo Alliaume <hugo@alliau.me>
2025-11-13 07:54:29 +0100

    [TwigComponent] Merge `profiler` and `profiler_collect_components` config options

--- src/TwigComponent/tests/Unit/DataCollector/TwigComponentDataCollectorTest.php
+++ src/TwigComponent/tests/Unit/DataCollector/TwigComponentDataCollectorTest.php
@@ -14,9 +14,16 @@ namespace Symfony\UX\TwigComponent\Tests\Unit\DataCollector;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\UX\TwigComponent\ComponentAttributes;
+use Symfony\UX\TwigComponent\ComponentMetadata;
 use Symfony\UX\TwigComponent\DataCollector\TwigComponentDataCollector;
+use Symfony\UX\TwigComponent\Event\PostRenderEvent;
+use Symfony\UX\TwigComponent\Event\PreRenderEvent;
 use Symfony\UX\TwigComponent\EventListener\TwigComponentLoggerListener;
+use Symfony\UX\TwigComponent\MountedComponent;
 use Twig\Environment;
+use Twig\Loader\ArrayLoader;
+use Twig\Runtime\EscaperRuntime;
 
 /**
  * @author Simon André <smn.andre@gmail.com>
@@ -35,7 +42,7 @@ class TwigComponentDataCollectorTest extends TestCase
         $this->assertSame([], $dataCollector->getData());
     }
 
-    public function testLateCollect()
+    public function testLateCollectWithNoCollectedData()
     {
         $logger = new TwigComponentLoggerListener();
         $twig = $this->createMock(Environment::class);
@@ -52,6 +59,44 @@ class TwigComponentDataCollectorTest extends TestCase
         $this->assertEmpty($dataCollector->getRenders());
 
         $this->assertEquals(0.0, $dataCollector->getRenderTime());
+    }
+
+    /**
+     * @testWith [true]
+     *           [false]
+     */
+    public function testLateCollectWithCollectedData(bool $collectComponents)
+    {
+        $logger = new TwigComponentLoggerListener();
+        $twig = new Environment(new ArrayLoader());
+        $dataCollector = new TwigComponentDataCollector($logger, $twig, $collectComponents);
+
+        // Trigger some events to be logged
+        $mounted = new MountedComponent('foo', new \stdClass(), new ComponentAttributes([], new EscaperRuntime()));
+        $eventA = new PreRenderEvent($mounted, new ComponentMetadata(['key' => 'foo', 'template' => 'bar']), []);
+        $logger->onPreRender($eventA);
+        $eventB = new PostRenderEvent($mounted);
+        $logger->onPostRender($eventB);
+
+        $dataCollector->lateCollect();
+
+        $this->assertSame(1, $dataCollector->getComponentCount());
+        $this->assertIsIterable($dataCollector->getComponents());
+        $this->assertNotEmpty($dataCollector->getComponents());
+
+        $this->assertSame(1, $dataCollector->getRenderCount());
+        $this->assertIsIterable($dataCollector->getRenders());
+        $this->assertNotEmpty($dataCollector->getRenders());
+
+        foreach ($dataCollector->getRenders() as $render) {
+            if ($collectComponents) {
+                $this->assertNotNull($render['component']);
+            } else {
+                $this->assertNull($render['component']);
+            }
+        }
+
+        $this->assertGreaterThan(0.0, $dataCollector->getRenderTime());
     }
 
     public function testReset()
