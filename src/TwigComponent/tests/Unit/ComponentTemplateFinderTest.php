a124d89e144ec0ef8d9843a4bd2ab105b534694f
andyexeter <palmer.andy@gmail.com>
2025-11-14 10:02:45 +0000

    Allow using directory name as component name for anonymous components

--- src/TwigComponent/tests/Unit/ComponentTemplateFinderTest.php
+++ src/TwigComponent/tests/Unit/ComponentTemplateFinderTest.php
@@ -117,6 +117,8 @@ final class ComponentTemplateFinderTest extends TestCase
             'foo/bar.html.twig',
             'bar/foo/bar.html.twig',
             'foo/foo/bar.html.twig',
+            'foo/qux/index.html.twig',
+            'foo/foo/baz/index.html.twig',
         ];
         $loader = $this->createLoader($templates);
         $finder = new ComponentTemplateFinder($loader, 'foo');
@@ -124,6 +126,9 @@ final class ComponentTemplateFinderTest extends TestCase
         $this->assertEquals('foo/bar.html.twig', $finder->findAnonymousComponentTemplate('bar'));
         $this->assertEquals('foo/foo/bar.html.twig', $finder->findAnonymousComponentTemplate('foo:bar'));
         $this->assertEquals('foo/foo/bar.html.twig', $finder->findAnonymousComponentTemplate('foo:bar'));
+
+        $this->assertEquals('foo/qux/index.html.twig', $finder->findAnonymousComponentTemplate('qux'));
+        $this->assertEquals('foo/foo/baz/index.html.twig', $finder->findAnonymousComponentTemplate('foo:baz'));
     }
 
     private function createLoader(array $templates): LoaderInterface
