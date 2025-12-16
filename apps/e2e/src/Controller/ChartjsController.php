72f692486c98b94c2e3166679e9c2873aad51ba5
Hugo Alliaume <hugo@alliau.me>
2025-12-16 08:03:38 +0100

    Add E2E tests for Chart.js

--- apps/e2e/src/Controller/ChartjsController.php
+++ apps/e2e/src/Controller/ChartjsController.php
@@ -58,4 +58,69 @@ final class ChartjsController extends AbstractController
             'chart' => $chart,
         ]);
     }
+
+    #[Route('/pie', name: 'pie')]
+    public function pie(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_PIE);
+
+        $chart->setData([
+            'labels' => ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            'datasets' => [
+                [
+                    'label' => 'My First Dataset',
+                    'data' => [12, 19, 3, 5, 2, 3],
+                    'backgroundColor' => [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(153, 102, 255)',
+                        'rgb(255, 159, 64)',
+                    ],
+                ],
+            ],
+        ]);
+
+        return $this->render('ux_chartjs/index.html.twig', [
+            'chart' => $chart,
+        ]);
+    }
+
+    #[Route('/pie-with-options', name: 'pie_with_options')]
+    public function pieWithOptions(ChartBuilderInterface $chartBuilder): Response
+    {
+        $chart = $chartBuilder->createChart(Chart::TYPE_PIE);
+
+        $chart->setData([
+            'labels' => ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            'datasets' => [
+                [
+                    'label' => 'My First Dataset',
+                    'data' => [12, 19, 3, 5, 2, 3],
+                    'backgroundColor' => [
+                        'rgb(255, 99, 132)',
+                        'rgb(54, 162, 235)',
+                        'rgb(255, 205, 86)',
+                        'rgb(75, 192, 192)',
+                        'rgb(153, 102, 255)',
+                        'rgb(255, 159, 64)',
+                    ],
+                ],
+            ],
+        ]);
+
+        $chart->setOptions([
+            'responsive' => true,
+            'plugins' => [
+                'legend' => [
+                    'position' => 'top',
+                ],
+            ],
+        ]);
+
+        return $this->render('ux_chartjs/index.html.twig', [
+            'chart' => $chart,
+        ]);
+    }
 }
