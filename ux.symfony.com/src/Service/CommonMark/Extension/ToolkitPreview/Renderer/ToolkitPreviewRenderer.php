3e2cdae0f9c362b4702d6bab7035097e0187d3bc
Hugo Alliaume <hugo@alliau.me>
2026-01-06 00:58:28 +0100

    [Site] Set dark/light theme of Toolkit preview iframes, given current page theme

--- ux.symfony.com/src/Service/CommonMark/Extension/ToolkitPreview/Renderer/ToolkitPreviewRenderer.php
+++ ux.symfony.com/src/Service/CommonMark/Extension/ToolkitPreview/Renderer/ToolkitPreviewRenderer.php
@@ -50,7 +50,13 @@ final readonly class ToolkitPreviewRenderer implements NodeRendererInterface
                 <svg width="18" height="18" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 1 1-6.219-8.56"/></svg>
                 <span>Loading...</span>
             </div>
-            <iframe class="Toolkit_Preview loading" src="{$previewUrl}" style="height: {$height};" loading="lazy" onload="this.previousElementSibling.style.display = 'none'; this.classList.remove('loading')"></iframe>
+            <iframe
+                class="Toolkit_Preview loading"
+                src="{$previewUrl}"
+                loading="lazy"
+                style="height: {$height};"
+                onload="this.previousElementSibling.style.display = 'none'; this.classList.remove('loading')"
+            ></iframe>
             HTML;
     }
 }
