ebdceee58ca847ba7bfe1082054be496475a5ba0
Allan Alf Nielsen <wertisdk@gmail.com>
2026-01-13 12:53:29 +0100

    [LiveComponent] Fix DataModelPropsSubscriber parsing LiveComponent modifiers as TwigComponent bindings
    The data-model attribute is used by both TwigComponent (for parent-child prop binding) and LiveComponent (for form input binding with modifiers). This was causing LiveComponent modifiers like 'norender|field' or 'debounce(100)|field' to be incorrectly parsed as TwigComponent bindings.
    
    This fix adds modifier detection to DataModelPropsSubscriber that checks if data-model contains LiveComponent modifier syntax (pipe '|' or function calls) and extracts the value from the modifier syntax
    
    Fixes #3152, #555

---
+++ src/LiveComponent/tests/Fixtures/Component/ParentComponentDataModelWithModifiers.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\UX\LiveComponent\Tests\Fixtures\Component;
+
+use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
+use Symfony\UX\LiveComponent\DefaultActionTrait;
+
+#[AsLiveComponent('parent_component_data_model_with_modifiers')]
+final class ParentComponentDataModelWithModifiers
+{
+    use DefaultActionTrait;
+}
