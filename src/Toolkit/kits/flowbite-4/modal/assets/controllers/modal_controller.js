bebc44f860c4ebec7c891bb83eb08576b9b01bea
DcgRG <dcg.dev@outlook.com>
2026-03-05 13:26:21 +0100

    [Toolkit][Flowbite] Add Modal component

---
+++ src/Toolkit/kits/flowbite-4/modal/assets/controllers/modal_controller.js
@@ -0,0 +1,43 @@
+import { Controller } from '@hotwired/stimulus';
+
+export default class extends Controller {
+    static targets = ['trigger', 'modal'];
+
+    open() {
+        this.modalTarget.showModal();
+
+        if (this.hasTriggerTarget) {
+            if (this.modalTarget.getAnimations().length > 0) {
+                this.modalTarget.addEventListener('transitionend', () => {
+                    this.triggerTarget.setAttribute('aria-expanded', 'true');
+                    this.modalTarget.setAttribute('aria-hidden', 'false');
+                });
+            } else {
+                this.triggerTarget.setAttribute('aria-expanded', 'true');
+                this.modalTarget.setAttribute('aria-hidden', 'false');
+            }
+        }
+    }
+
+    closeOnClickOutside({ target }) {
+        if (target === this.modalTarget) {
+            this.close();
+        }
+    }
+
+    close() {
+        this.modalTarget.close();
+
+        if (this.hasTriggerTarget) {
+            if (this.modalTarget.getAnimations().length > 0) {
+                this.modalTarget.addEventListener('transitionend', () => {
+                    this.triggerTarget.setAttribute('aria-expanded', 'false');
+                    this.modalTarget.setAttribute('aria-hidden', 'true');
+                });
+            } else {
+                this.triggerTarget.setAttribute('aria-expanded', 'false');
+                this.modalTarget.setAttribute('aria-hidden', 'true');
+            }
+        }
+    }
+}
