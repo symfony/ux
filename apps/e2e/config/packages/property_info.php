34ee8dc69ee25203ceab79b32c0b76721e650a4f
Hugo Alliaume <hugo@alliau.me>
2026-03-21 13:44:37 +0100

    [E2E] Test over Symfony 8.0

--- apps/e2e/config/packages/property_info.php
+++ apps/e2e/config/packages/property_info.php
@@ -1,10 +1,17 @@
 <?php
 
 use App\Kernel;
-use Symfony\Config\FrameworkConfig;
 
-return static function (FrameworkConfig $config) {
-    if (Kernel::VERSION_ID >= 70000) {
+if (Kernel::VERSION_ID >= 80000) {
+    return Symfony\Component\DependencyInjection\Loader\Configurator\App::config([
+        'framework' => [
+            'property_info' => [
+                'with_constructor_extractor' => true,
+            ]
+        ]
+    ]);
+} else if (Kernel::VERSION_ID >= 70000) {
+    return static function (Symfony\Config\FrameworkConfig $config) {
         $config->propertyInfo()->withConstructorExtractor(true);
-    }
-};
+    };
+}
