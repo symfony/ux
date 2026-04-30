52e285fe173e366e1952daccf02ceda33915e757
Hamza Makraz <hamza@makraz.com>
2026-04-30 10:30:00 +0100

    [Editor] Add Page builder format abstracts

---
+++ src/Editor/src/Bridge/Format/Page/PageCapabilities.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\UX\Editor\Bridge\Format\Page;
+
+use Symfony\UX\Editor\Config\BridgeCapabilities;
+
+final class PageCapabilities
+{
+    public static function default(): BridgeCapabilities
+    {
+        return new BridgeCapabilities(
+            supportsToolbar: false,
+            supportsPlugins: true,
+            supportsTheme: true,
+            supportsLanguage: true,
+            supportedFormats: ['page'],
+        );
+    }
+}
---
+++ src/Editor/src/Bridge/Format/Page/PageSandboxRenderer.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Symfony\UX\Editor\Bridge\Format\Page;
+
+use Symfony\UX\Editor\Content\PageContent;
+
+final class PageSandboxRenderer
+{
+    public function __construct(private readonly string $sandbox = 'allow-same-origin')
+    {
+    }
+
+    public function render(PageContent $page): string
+    {
+        if ($page->isEmpty()) {
+            return '';
+        }
+        $srcdoc = sprintf(
+            '<!doctype html><html><head><style>%s</style></head><body>%s</body></html>',
+            $page->css,
+            $page->html,
+        );
+
+        return sprintf(
+            '<iframe sandbox="%s" srcdoc="%s"></iframe>',
+            htmlspecialchars($this->sandbox, \ENT_QUOTES | \ENT_HTML5, 'UTF-8'),
+            htmlspecialchars($srcdoc, \ENT_QUOTES | \ENT_HTML5, 'UTF-8'),
+        );
+    }
+}
