a803dd4a3207e2827d585813446f4d1c2485db3c
luigif <luigif@gmail.com>
2026-08-25 16:20:57 +0200

    Translate 'Add %placeholder%' to Italian
    Italian translation was missing, it's still missing in some other languages

--- src/Autocomplete/translations/AutocompleteBundle.it.php
+++ src/Autocomplete/translations/AutocompleteBundle.it.php
@@ -13,5 +13,5 @@ return [
     'Loading more results...' => 'Caricamento di altri risultati...',
     'No results found' => 'Nessun risultato trovato',
     'No more results' => 'Non ci sono altri risultati',
-    // 'Add %placeholder%...' => 'Add %placeholder%...',
+    'Add %placeholder%...' => 'Aggiungi %placeholder%...',
 ];
